605944eaccf7c1d3f7eec63321790046750a3371
sacveras <sabrina_veras@live.com>
2022-05-08 20:04:56 -0300

    create the rounds for the game

--- src/pages/Game/GamePage.tsx
+++ src/pages/Game/GamePage.tsx
@@ -1,30 +1,78 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import faker from "@faker-js/faker";
 import { GamePageView } from "./GamePageView";
-
-export interface PlayerProps {
-  name: string;
-}
+import { Player } from "domain/Player";
 
 export function GamePage() {
-  const [players, setPlayers] = React.useState<PlayerProps[]>([]);
-  // const [eliminatePlayers, setEliminatePlayers] = React.useState<PlayerProps[]>(
-  //   []
-  // );
-  // const [totalAmount, setTotalAmount] = React.useState<number>(0);
+  const navigate = useNavigate();
+
+  const [round, setRound] = React.useState<number>(0);
+  const [remainingPlayers, setRemainingPlayers] = React.useState<Player[]>([]);
+  const [eliminatePlayers, setEliminatePlayers] = React.useState<Player[]>([]);
+  const [votesEndGame, setVotesEndGame] = React.useState<number>(0);
 
   React.useEffect(() => {
-    setPlayers(addPlayer());
+    setRemainingPlayers(addPlayer());
   }, []);
 
   function addPlayer() {
-    let newPlayers: PlayerProps[] = [];
+    let newPlayers: Player[] = [];
     for (let count = 0; count < 69; count++) {
-      newPlayers.push({ name: faker.name.findName() });
+      newPlayers.push({ name: `${count} ${faker.name.findName()}` } as Player);
     }
 
     return newPlayers;
   }
 
-  return <GamePageView players={players} />;
+  async function calcVotes(players: Player[]) {
+    // During voting, each player has a change of voting Yes or No
+    // The change that a player chooses to end the game is 30%
+    const count = players.filter((player) => {
+      const porcentagem = Math.floor(Math.random() * 101);
+      if (porcentagem <= 30) {
+        return player;
+      }
+
+      return undefined;
+    });
+
+    return count.length;
+  }
+
+  async function newRound() {
+    setRound(round + 1);
+    setVotesEndGame(0);
+    // each round the player has a 42% chance of being eliminated
+    const players = remainingPlayers.filter((player) => {
+      const porcentagem = Math.floor(Math.random() * 101);
+      if (porcentagem <= 42) {
+        setEliminatePlayers((oldEliminatePlayers) => [
+          ...oldEliminatePlayers,
+          player,
+        ]);
+        return undefined;
+      } else {
+        return player;
+      }
+    });
+    setRemainingPlayers(players);
+    const votes = await calcVotes(players);
+    setVotesEndGame(votes);
+
+    if (players.length === 0 || votes >= players.length / 2) {
+      //end the game
+      navigate("/end-game");
+    }
+  }
+
+  return (
+    <GamePageView
+      remainingPlayers={remainingPlayers}
+      eliminatePlayers={eliminatePlayers}
+      round={round}
+      votesEndGame={votesEndGame}
+      newRound={newRound}
+    />
+  );
 }
--- src/pages/Game/GamePageView.tsx
+++ src/pages/Game/GamePageView.tsx
@@ -1,25 +1,125 @@
 import styled from "styled-components";
-import { PlayerProps } from "./GamePage";
+import * as ds from "design";
+
+import { Player } from "domain/Player";
+import { PlayerList } from "domain/Player/components/PlayerList";
 
 export interface GamePageViewProps {
-  players: PlayerProps[];
+  remainingPlayers: Player[];
+  eliminatePlayers: Player[];
+  round: number;
+  votesEndGame: number;
+  newRound: () => void;
 }
 
-export function GamePageView({ players }: GamePageViewProps) {
+export function GamePageView({
+  remainingPlayers,
+  eliminatePlayers,
+  round,
+  votesEndGame,
+  newRound,
+}: GamePageViewProps) {
   return (
     <GamePageViewWrapper>
-      <ul>
-        {players.map((player) => (
-          <li key={player.name.toString()}>{player.name.toString()}</li>
-        ))}
-      </ul>
+      <div>
+        <PlayerList
+          players={remainingPlayers}
+          title={"Jogadores Remanescentes"}
+        />
+      </div>
+      <GameInfo
+        round={round}
+        eliminatePlayers={eliminatePlayers}
+        votesEndGame={votesEndGame}
+        newRound={newRound}
+      />
+      <div>
+        <PlayerList players={eliminatePlayers} title={"Jogadores Eliminados"} />
+      </div>
     </GamePageViewWrapper>
   );
 }
 
+function GameInfo({
+  round,
+  eliminatePlayers,
+  votesEndGame,
+  newRound,
+}: {
+  round: number;
+  eliminatePlayers: Player[];
+  votesEndGame: number;
+  newRound: () => void;
+}) {
+  return (
+    <GameInfoWrapper>
+      <div style={{ display: "flex", flexDirection: "column", gap: "16px" }}>
+        <RoundWrapper>Round: {round}</RoundWrapper>
+
+        <AmountWrapper>
+          Fundo do Prêmio: {420 * eliminatePlayers.length}
+        </AmountWrapper>
+      </div>
+
+      <div style={{ margin: "24px 0px" }}>
+        <ds.icons.SoldierSquare />
+      </div>
+
+      <div>Votos par ao fim do jogo: {votesEndGame}</div>
+
+      <div style={{ marginTop: "24px" }}>
+        <ds.components.button.Button onClick={() => newRound()} width={"180px"}>
+          {round === 0 ? "Iniciar Rodada" : "Nova Rodada"}
+        </ds.components.button.Button>
+      </div>
+    </GameInfoWrapper>
+  );
+}
+
+const GameInfoWrapper = styled.div`
+  font-family: "Nunito", sans-serif;
+  font-size: 24px;
+  font-weight: 600;
+  color: #ffffff;
+
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const AmountWrapper = styled.div`
+  font-family: "Nunito", sans-serif;
+  font-size: 20px;
+  font-weight: 600;
+  color: #ffffff;
+`;
+
+const RoundWrapper = styled.div``;
+
 const GamePageViewWrapper = styled.div`
-  width: 100%;
+  width: calc(100% - 48px);
   height: 100vh;
+  background-color: #282c34;
+
+  display: grid;
+  grid-template-columns: 400px 1fr 400px;
+  grid-template-rows: 1fr;
+
+  padding: 24px;
 
   overflow-y: scroll;
+  overflow-x: hidden;
+  &::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  &::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background-color: #282c34;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: #0c0c0c;
+  }
 `;
