be250e534753271f8aee43728cc8d534a15c9bbe
sacveras <sabrina_veras@live.com>
2022-05-09 10:24:24 -0300

    add new icon
    SoldierCircle

--- src/design/icons.ts
+++ src/design/icons.ts
@@ -1,7 +1,9 @@
 import { SquidGameLogo } from "./icons/SquidGameLogo";
 import { SoldierSquare } from "./icons/SoldierSquare";
+import { SoldierCircle } from "./icons/SoldierCircle";
 
 export const icons = {
   SquidGameLogo,
   SoldierSquare,
+  SoldierCircle,
 };
