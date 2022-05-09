36c06cdd73f5493b8a5cc68699a8f6f75d04da3c
sacveras <sabrina_veras@live.com>
2022-05-08 21:23:50 -0300

    refactoring game page
    add hooks components

--- src/pages/Game/GamePage.tsx
+++ src/pages/Game/GamePage.tsx
@@ -1,66 +1,32 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import faker from "@faker-js/faker";
+import { useRounds } from "hooks/useRounds";
+import { useVotes } from "hooks/useVotes";
+import { usePlayers } from "hooks/usePlayers";
 import { GamePageView } from "./GamePageView";
-import { Player } from "domain/Player";
 
 export function GamePage() {
   const navigate = useNavigate();
 
-  const [round, setRound] = React.useState<number>(0);
-  const [remainingPlayers, setRemainingPlayers] = React.useState<Player[]>([]);
-  const [eliminatePlayers, setEliminatePlayers] = React.useState<Player[]>([]);
-  const [votesEndGame, setVotesEndGame] = React.useState<number>(0);
+  const { remainingPlayers, eliminatePlayers, addPlayers, removePlayers } =
+    usePlayers();
+  const { round, setNewRound } = useRounds();
+  const { votesEndGame, clearVotes, calcVotes } = useVotes(remainingPlayers);
 
   React.useEffect(() => {
-    setRemainingPlayers(addPlayer());
+    addPlayers();
   }, []);
 
-  function addPlayer() {
-    let newPlayers: Player[] = [];
-    for (let count = 0; count < 69; count++) {
-      newPlayers.push({ name: `${count} ${faker.name.findName()}` } as Player);
-    }
-
-    return newPlayers;
-  }
-
-  async function calcVotes(players: Player[]) {
-    // During voting, each player has a change of voting Yes or No
-    // The change that a player chooses to end the game is 30%
-    const count = players.filter((player) => {
-      const porcentagem = Math.floor(Math.random() * 101);
-      if (porcentagem <= 30) {
-        return player;
-      }
-
-      return undefined;
-    });
-
-    return count.length;
-  }
-
   async function newRound() {
-    setRound(round + 1);
-    setVotesEndGame(0);
-    // each round the player has a 42% chance of being eliminated
-    const players = remainingPlayers.filter((player) => {
-      const porcentagem = Math.floor(Math.random() * 101);
-      if (porcentagem <= 42) {
-        setEliminatePlayers((oldEliminatePlayers) => [
-          ...oldEliminatePlayers,
-          player,
-        ]);
-        return undefined;
-      } else {
-        return player;
-      }
-    });
-    setRemainingPlayers(players);
-    const votes = await calcVotes(players);
-    setVotesEndGame(votes);
+    setNewRound();
+    clearVotes();
+    removePlayers();
+    calcVotes(remainingPlayers);
 
-    if (players.length === 0 || votes >= players.length / 2) {
+    if (
+      remainingPlayers.length === 0 ||
+      votesEndGame >= remainingPlayers.length / 2
+    ) {
       //end the game
       navigate("/end-game");
     }
